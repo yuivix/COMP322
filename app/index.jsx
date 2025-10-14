785c0fad507c5db2ec2f7d5d65a4aed04544ec03
Matija-B <mbebek@luc.edu>
2025-10-14 13:44:17 -0500

    Did screen 1 need to fix bugs

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native'
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 //import for our text
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, StatusBar, Button, TouchableHighlight } from 'react-native'
 //import react, make a module called Component
 import React, {Component} from 'react'
+
+
+const Stack = createNativeStackNavigator();
+
 
 
 //This is our first basicApp
@@ -15,11 +22,35 @@ class Header extends Component {
     }
 }
 
+class Box1 extends Component{
+  render() {
+    return (
+      <View style = {styles.box}>
+        <Text style = {styles.boxWriting}> {this.props.label}</Text>
+      </View>
+    );
+  }
+}
+
 class Screen extends Component {
+
+  secondScreen  = () => {this.props.navigation.navigate("Pantry Pal")}
     render() {
       return (
         <View style={styles.container}>
-          <Header heading="Pantry Pal" />
+          <Header heading = "Pantry Pal" />
+
+          <View style={styles.boxSpace}>
+            <Box1 label = "Dairy" />
+            <Box1 label = "Vegtable" />
+            <Box1 label = "Fruit" />
+            <Box1 label = "Meat" />
+            <Box1 label = "Start" />
+            <Box1 label = "Cheese" />
+            <Box1 label = "Spices" />
+            <Box1 label = "Exotic Fruit" />
+            <Box1 label = "Snacks" />
+          </View>
         </View>
         ); 
     }
@@ -27,7 +58,22 @@ class Screen extends Component {
 
 
 
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name = "Pantry Pal" component= {Screen} />
+          <Stack.Screen name = "Shining above the Spire" component={Screen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    ); 
+  }
+}
+
 export default Screen;
+
+
 
 
 const styles = StyleSheet.create({
@@ -44,9 +90,31 @@ const styles = StyleSheet.create({
             color: '#080808',
             fontSize: 22,
             textAlign: 'center',
-        } 
-});
+        },
+        boxSpace: {
+          flex: 1,
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+          paddingLeft: 300,
+          paddingRight: 300,
+          paddingTop: 20,
+          paddingBottom: 250,
+        },
+        box:{
+          height: '50%',
+          width: '30%',
+          backgroundColor: '#0000FF',
+          opacity: '10%',
+          marginBottom: '3%',
 
+        },
+        boxWriting: {
+          fontSize: 18,
+          color: '#080808',
+        }
+         
+});
 
 
 
---
+++ app/mainPage.jsx
@@ -0,0 +1,49 @@
+import { NavigationContainer } from '@react-navigation/native'
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import for our text
+import { StyleSheet, Text, View } from 'react-native'
+//import react, make a module called Component
+import React, {Component} from 'react'
+
+class Header extends Component {
+    render() {
+      return (
+        <View style={styles.header}>
+          <Text style={styles.heading}>{ this.props.heading }</Text>
+        </View>
+        ); 
+    }
+}
+
+// screen container
+class Screen2 extends Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Header heading="Shining above the Spire" />
+        </View>
+    ); 
+  }
+}
+
+export default screen;
+
+const styles = StyleSheet.create({
+    container: {
+  flex: 1,
+      backgroundColor: '#CCCCCC'
+    },
+    header: {
+      height: '20%',
+      backgroundColor: '#333333',
+      padding: 50
+  }, heading: {
+      color: '#DEDEDE',
+      fontSize: 22,
+      textAlign: 'center',
+  } });
+
+
+
+
